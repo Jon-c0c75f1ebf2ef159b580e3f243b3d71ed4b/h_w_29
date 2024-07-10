4b540ef85d51388078123c3eab675df906d4ccfd
Jon <148043487+Jon-c0c75f1ebf2ef159b580e3f243b3d71ed4b@users.noreply.github.com>
2024-07-11 01:12:19 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,22 @@
 #include "FineGrainedQueue.h"
+#include <thread>
 
 int main()
 {
-    FineGrainedQueue queue(new Node{11, new Node(22, nullptr) } );
+    FineGrainedQueue queue(new Node(10, nullptr));
+
+    std::thread t1([&queue]() { queue.insertIntoMiddle(20, 15); });
+    std::thread t2([&queue]() { queue.insertIntoMiddle(30, 15); });
+    std::thread t3([&queue]() { queue.insertIntoMiddle(40, 15); });
+    std::thread t4([&queue]() { queue.insertIntoMiddle(50, 15); });
+    std::thread t5([&queue]() { queue.insertIntoMiddle(60, 15); });
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+    t5.join(); 
     
-    queue.insertIntoMiddle(33, 2);
-    queue.insertIntoMiddle(89, 2);
-    queue.insertIntoMiddle(4, 89);
-    queue.insertIntoMiddle(1655, 2);
-    queue.insertIntoMiddle(99, 2);
+    queue._cout();
 
     return 0;
 }
