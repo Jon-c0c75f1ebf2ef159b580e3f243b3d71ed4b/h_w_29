0d847dd4869abee82b2af4af335bb2542f1654fa
Jon <148043487+Jon-c0c75f1ebf2ef159b580e3f243b3d71ed4b@users.noreply.github.com>
2024-07-11 03:03:26 +0300

    Update FineGrainedQueue.h

--- FineGrainedQueue.h
+++ FineGrainedQueue.h
@@ -27,33 +27,52 @@ public:
     FineGrainedQueue(Node* node) : _head(node) { _queue_mutex = new std::mutex; };
     ~FineGrainedQueue() { delete _queue_mutex; }
     void insertIntoMiddle(int value, int pos) {
-        std::unique_lock<std::mutex> guard(*(_queue_mutex));
-        Node* insertNode = new Node{ value, nullptr };
         //очередь не пустая
         //вставляется элемент в середину или конец списка, то есть вставку в начало списка не нужно рассматривать
-        //соответственно начинаем со второй позиции
-        Node* cur = _head->_next;
-        Node* tmp = _head;
+        //соответственно начинаем со второй позиции i=0; ++i;
+        Node* cur, * prev;
         int i = 1;
-        while (true){
+
+        _queue_mutex->lock();
+        prev = _head; 
+        cur = prev->_next;
+        prev->_node_mutex->lock();
+        _queue_mutex->unlock();
+        if (cur) {// проверили и только потом залочили
+            cur->_node_mutex->lock();
+        }
+        
+
+        while (true) {
             //если pos больше длины списка, то нужно вставить элемент в конец списка.
             if (i >= pos || cur == nullptr) {
                 //insert
-                tmp->_next = insertNode;
-                insertNode->_next = cur;
+                prev->_next = new Node{ value, cur };                
+                if (cur) {// проверили и только потом залочили
+                    cur->_node_mutex->unlock();
+                }
+                prev->_node_mutex->unlock();
                 break;
             }
             ++i;
-            tmp = cur;
-            cur = cur->_next;
+
+            Node* old_prev = prev;
+            prev = cur;
+            cur = cur->_next;            
+
+            if (cur) {// проверили и только потом залочили
+                cur->_node_mutex->lock();
+            }
+            old_prev->_node_mutex->unlock();
         }
     }
-    
+
     void _cout() {
+        std::unique_lock<std::mutex> guard(*(_queue_mutex));
         Node* cur = _head;
-        while (cur != nullptr){
-        std::cout << cur->_value << "\n";
-        cur = cur->_next;
+        while (cur != nullptr) {
+            std::cout << cur->_value << "\n";
+            cur = cur->_next;
         }
     }
 };
