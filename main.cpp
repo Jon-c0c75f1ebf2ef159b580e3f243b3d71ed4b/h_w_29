76a4660acd21a371dda4b22940e2472d1d89669e
Jon <148043487+Jon-c0c75f1ebf2ef159b580e3f243b3d71ed4b@users.noreply.github.com>
2024-07-11 11:29:26 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@
 
 int main()
 {
-    FineGrainedQueue queue(new Node(10, nullptr));
+    FineGrainedQueue queue(new Node{ 10, new Node(20, nullptr) });
 
-    std::thread t1([&queue]() { queue.insertIntoMiddle(20, 15); });
-    std::thread t2([&queue]() { queue.insertIntoMiddle(30, 15); });
-    std::thread t3([&queue]() { queue.insertIntoMiddle(40, 15); });
-    std::thread t4([&queue]() { queue.insertIntoMiddle(50, 15); });
-    std::thread t5([&queue]() { queue.insertIntoMiddle(60, 15); });
+    std::thread t1([&queue]() { queue.insertIntoMiddle(30, 2); });
+    std::thread t2([&queue]() { queue.insertIntoMiddle(40, 4); });
+    std::thread t3([&queue]() { queue.insertIntoMiddle(50, 11); });
+    std::thread t4([&queue]() { queue.insertIntoMiddle(60, 25); });
+    std::thread t5([&queue]() { queue.insertIntoMiddle(70, 3); });
     t1.join();
     t2.join();
     t3.join();
